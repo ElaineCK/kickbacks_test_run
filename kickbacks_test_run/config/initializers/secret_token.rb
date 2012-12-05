8aefba3972ff2fc1782302ac8ff395975b23964e
Elaine Marino & Jessica Goulding <pair+elaine+jessica@commercekitchen.com>
2012-12-05 14:57:38 -0700

    First Rails App for Kickback

---
+++ kickbacks_test_run/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickbacksTestRun::Application.config.secret_token = 'c0130236138c2e432c3cf55f01a40e12ec522d3d5c219a2947e04804f8d38033dc06497de94ec808c5a5b72958d382ad0a1ebc041ed07dc4d35ce5641f685d42'
