f9c845409948727e5c293d5539a39183dc7787a2
Elaine Marino & Jessica Goulding <pair+elaine+jessica@commercekitchen.com>
2012-12-10 13:29:09 -0700

    Added pages

--- kickbacks_test_run/config/routes.rb
+++ kickbacks_test_run/config/routes.rb
@@ -1,6 +1,12 @@
 KickbacksTestRun::Application.routes.draw do
 
   root(:to => 'searches#index')
+
+  get 'searches/bio' => 'searches#bio', :as => 'bio'
+  get 'searches/videos' => 'searches#videos', :as => 'videos'
+  get 'searches/fanbase' => 'searches#fanbase', :as => 'fanbase'
+  get 'searches/about' => 'searches#about', :as => 'about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
